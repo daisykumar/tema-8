20aa18379a10c1021ad211f96aba3b8f90e2f720
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-04-22 13:05:51 +0200

    c

--- profile.vanilla/index.js
+++ profile.vanilla/index.js
@@ -22,7 +22,7 @@ function greet(){
     newOKButton.innerHTML = 'ok'
 
     newOKButton.addEventListener('click', function(){
-        form.innerHTML = '<h1>Supert!</h1><p>Hva er din favoritt aktiviter? (Obligatorisk)</p><div><input type="checkbox" id="mat" name="interest" value="mat"><label for="matLaging">Mat laging</label></div><input type="checkbox" id="tv" name="interest" value="se på tv"><label for="tv">Se på tv</label></div><input type="checkbox" id="skog" name="interest" value="skogstur"><label for="skogstur">Skogstur</label></div><p>En rekke informasjoner/spørsmål vi gjerne vil ha fra brukerne men det er optional:</p><label for="antallVenner">Antall venner på skolen?</label><input id="textInput" class="custom" size="32"><label for="totalVenner">Antall venner alt i alt??</label><input id="textInput" class="custom" size="32">'
+        form.innerHTML = '<h1>Supert!</h1><p>Hva er din favoritt aktiviter? (Obligatorisk)</p><div><input type="checkbox" id="mat" name="interest" value="mat"><label for="matLaging">Mat laging</label></div><input type="checkbox" id="tv" name="interest" value="se på tv"><label for="tv">Se på tv</label></div><input type="checkbox" id="skog" name="interest" value="skogstur"><label for="skogstur">Skogstur</label></div><p>En rekke informasjoner/spørsmål vi gjerne vil ha fra brukerne men det er optional:</p><label for="antallVenner">Antall venner på skolen?</label><input id="textInput" class="custom" size="32"><label for="totalVenner">Antall venner alt i alt??</label><input id="textInput" class="custom" size="32"><button type="enter">Submit form</button>'
     })
 
     form.appendChild(newOKButton)

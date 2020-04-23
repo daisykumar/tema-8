74b60bb8cf117c2b57300fd2d33338c1fb333ee9
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-04-23 10:31:06 +0200

    c

---
+++ arrows.function/index.js
@@ -0,0 +1,28 @@
+console.log('I am here')
+
+function square(tall){
+    return name + ', regnestykket ditt gir: ' + tall * tall
+}
+
+console.log( square(16, 'Per') )
+
+
+const squareA = tall =>  tall * tall
+
+
+console.log(squareA(21))
+
+
+const fler = (name1, name2) => 'Hei ' + name1 + ' og ' + name2 
+
+console.log(fler('Simon', 'Per'))
+
+setTimeout(()=> document.querySelector('body').style.backgroundColor='orange', 2000)
+
+const antallTegn = ord => 'Dette od har ' + ord.length + ' karakterer'
+
+console.log(antallTegn('nikeodemos'))
+
+const tallene = [
+    12, 3, 4, 56, 67, 44, 34, 32
+]

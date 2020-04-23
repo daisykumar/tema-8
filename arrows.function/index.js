b0e8bc876bd36f5e644ceec9ed2a1f64644d7c61
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-04-23 10:54:00 +0200

    c

--- arrows.function/index.js
+++ arrows.function/index.js
@@ -26,3 +26,27 @@ console.log(antallTegn('nikeodemos'))
 const tallene = [
     12, 3, 4, 56, 67, 44, 34, 32
 ]
+
+//for(i=0;i < tallene.length - 1; i++){
+    //console.log(tallene[i])
+//}
+
+let body = document.querySelector('body')
+tallene.map( tall =>  
+    {
+        let newLi=document.createElement('li')
+        newLi.innerHTML = tall
+        body.appendChild(newLi)
+
+    }
+)
+
+
+const ordene = ['lover', 'katter', 'elefanter', 'prinser']
+let str = ''
+
+ordene.map( ord => {
+    str += `<section>Det satt 2 ${ord}</section>`
+
+}
+)

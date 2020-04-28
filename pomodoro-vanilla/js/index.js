e8afc43a746baaf49f7a90872aee7d3a0bb8db9c
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-04-28 12:30:44 +0200

    update

--- pomodoro-vanilla/js/index.js
+++ pomodoro-vanilla/js/index.js
@@ -2,6 +2,61 @@
 var timeTextObject = document.querySelector('.time')
 var unitTextObject = document.querySelector('.unit')
 var startButton    = document.querySelector('.button')
+let timer
+
+let time = {
+    minutes:25,
+    seconds:0
+}
+
+startButton.addEventListener('click', ()=>{
+    startButton.classList.add('hidden')
+    timer = setInterval(tick, 1000)
+    showDuration()
+})
+
+
+//appens 'motor', runs after evert sec
+const tick = () => {
+    if(time.minutes == 0 && time.seconds == 0){
+        alarm()
+    }else{
+        countDown()
+        showDuration()
+    }
+}
+
+const alarm = () =>{
+    clearInterval(timer)
+    timeTextObject.innerHTML = 'AlARM'
+    setTimeout(()=>{
+        time = {
+            minutes:25,
+            seconds:0
+    }
+    startButton.classList.remove('hidden')
+    unitTextObject.classList.remove('hidden')
+    timeTextObject.innerHTML = '25'
+    }, 5000)
+}
+
+
+//vis tiden 
+const showDuration = () => {
+    let minutes = time.minutes < 10 ? '0' + time.minutes : time.minutes
+    let seconds = time.seconds < 10 ? '0' + time.seconds : time.seconds
+    timeTextObject.innerHTML = `${minutes}:${seconds}`
+
+}
+
+//telle tiden ned
+const countDown = () => {
+    time.seconds = time.seconds - 1
+    if(time.seconds < 0){
+        time.minutes = time.minutes -1
+        time.seconds = 59
+    }
+}
 
 
 
@@ -20,7 +75,7 @@ var startButton    = document.querySelector('.button')
 //sjekk om alarmen skal ringe - reset timer og tidsobjekt 
 //ellers, kald countdown og showduration
 
-//oprett en funksjon, showduration som viser tidfen
+//oprett en funksjon, showduration som viser tiden
 //hvis sekunder er mindre end 10  sett et 0 inn foran  
 //hvis minutter er mindre end 10  sett et 0 inn foran  
 

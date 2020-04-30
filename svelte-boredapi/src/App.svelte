c07d190f66a055bfa41c08c7e7ae7ddf7ee25a83
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-04-30 11:22:53 +0200

    c

--- svelte-boredapi/src/App.svelte
+++ svelte-boredapi/src/App.svelte
@@ -1,30 +1,27 @@
 <script>
-	export let name;
+
+	let name = 'Per'
+	let show = false	
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if show}
+	<h1>Hei {name}</h1>
+
+	{:else}
+		<h2>nothing to show</h2>
+	{/if}
+	<input type="text" bind:value={name}>
+	<button on:click={ () => show= !show}>Vis Hilsen</button>
+
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+/* */
+	main{
+		display: grid;
+		place-items: center;
+		height: 100vh;
 	}
 </style>

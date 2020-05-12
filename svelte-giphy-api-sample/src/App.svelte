1d3fc3fe616d7afb6a6784d64bb4964bcff6aafd
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-05-12 10:42:54 +0200

    gify-sample

---
+++ svelte-giphy-api-sample/src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+	let api_key = 'WyQy5ArEi0aCVW7jcuqPheIw6icalIIE'
+  	let q = ''
+	const limit = 1
+	let gif
+	 
+	 const getgif = () =>{
+	 fetch(`https://api.giphy.com/v1/gifs/search?q=${q}&limit=${limit}&api_key=${api_key}`) 
+		.then( res => res.json() )
+			.then( json => {
+			 console.log(json)
+			 gif = json.data[0].images.downsized_medium.url 
+			})
+}
+</script>
+
+
+<main>
+
+<header>
+	<input placeholder="Search for gif" type="text" bind:value={q} on:keydown={(key) => key.key == "Enter"? getgif():''}>
+	<button on:click={getgif}>Søk</button>
+</header>
+	
+{#if gif}
+    <img src="{gif}" alt="{q}">
+{:else}
+    <h2>Skriv noe og trykk Søk</h2>
+{/if}   
+</main>
+
+<style>
+	:global(body, html){
+		margin:0;
+		padding:0;
+	}
+	:global(*){
+		box-sizing:border-box;
+	}
+	main{
+		display:grid;
+		place-items:center;
+		height:100%;
+	}
+	header{
+		position:absolute;
+		top:2rem;
+		width:100%;
+		display:grid;
+		padding: 0 20vw 0 20vw;
+	
+	}
+</style>

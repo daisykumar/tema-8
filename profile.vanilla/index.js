f1499406b82e4d3e15b21e65e95ed9ff9241039a
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-05-15 11:10:43 +0200

    change

--- profile.vanilla/index.js
+++ profile.vanilla/index.js
@@ -22,7 +22,17 @@ function greet(){
     newOKButton.innerHTML = 'ok'
 
     newOKButton.addEventListener('click', function(){
-        form.innerHTML = '<h1>Supert!</h1><p>Hva er din favoritt aktiviter? (Obligatorisk)</p><div><input type="checkbox" id="mat" name="interest" value="mat"><label for="matLaging">Mat laging</label></div><input type="checkbox" id="tv" name="interest" value="se på tv"><label for="tv">Se på tv</label></div><input type="checkbox" id="skog" name="interest" value="skogstur"><label for="skogstur">Skogstur</label></div><p>En rekke informasjoner/spørsmål vi gjerne vil ha fra brukerne men det er optional:</p><label for="antallVenner">Antall venner på skolen?</label><input id="textInput" class="custom" size="32"><label for="totalVenner">Antall venner alt i alt??</label><input id="textInput" class="custom" size="32"><button type="enter">Submit form</button>'
+        form.innerHTML = '<h1>Supert!</h1>'
+        form.innerHTML +=`<p> Hva er din favoritt aktiviter? (Obligatorisk)`
+        form.innerHTML += '<input type="checkbox" id="mat" name="interest" value="mat"> <label for="matLaging">Mat laging</label>'
+        form.innerHTML += '<input type="checkbox" id="tv" name="interest" value="se på tv"><label for="tv">Se på tv</label>'
+        form.innerHTML += '<input type="checkbox" id="skog" name="interest" value="skogstur"><label for="skogstur">Skogstur</label>'
+        form.innerHTML += '<p>En rekke informasjoner/spørsmål vi gjerne vil ha fra brukerne men det er optional:</p>'
+        form.innerHTML += ' <label for="antallVenner">Antall venner på skolen?</label>'
+        form.innerHTML += '<input id="textInput" class="custom" size="32">'
+        form.innerHTML += '<label for="totalVenner">Antall venner alt i alt??</label>'
+        form.innerHTML += '<input id="textInput" class="custom" size="32">'
+        form.innerHTML += '<button type="enter">Submit form</button>'
     })
 
     form.appendChild(newOKButton)

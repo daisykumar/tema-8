699cabf8c317064b2746c6f104cd0e41325f289f
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-05-20 08:47:11 +0200

    electron-svelte-template

---
+++ electron-svelte-basic/src/App.svelte
@@ -0,0 +1,92 @@
+<script>
+	import {fly } from 'svelte/transition'
+	import { time } from './stores.js'
+
+	let alarm = new Date(1000)
+	let visbeskjed = false
+	let img = './assets/is.png'
+	let setMinutes = false
+
+	/*const formatter = new Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit',
+		second: '2-digit'
+	})*/
+
+	
+	$: beskjed = kanjegfaais ? 'Ja, du kan få is' : 'Nei, og nå må du vente enda lengere'
+
+	$: kanjegfaais = alarm < $time ? true : false
+
+	let foreldreKontroll = 30
+
+	const faaIs = (event) => {
+
+		visbeskjed = true
+		
+		if(kanjegfaais){
+			event.target.src = './assets/flag.webp'
+		}else{
+			event.target.src = './assets/barn.webp'
+		}
+		
+		setTimeout(()=>{
+			event.target.src = './assets/is.png'
+			visbeskjed = false
+			alarm = new Date(new Date().getTime() + foreldreKontroll * 60000)
+		}, 5000)
+	}
+
+</script>
+	<svelte:window on:keydown={ e => e.key == 'f' ? setMinutes = !setMinutes : ' ' }/>
+	{#if setMinutes}
+		<div transition:fly={{x:-1000}} class="set">
+			<div>
+				<h2>Hvor mange minutter kjære forelder?</h2>
+				<input bind:value={foreldreKontroll} on:keydown={e=>e.key=='Enter' ? setMinutes=false : ''}/>
+				<button on:click={()=>setMinutes=false} >ok</button>
+			</div>
+		</div>
+	{/if}
+<main>
+	<img src='./assets/is.png' alt='iskrem' on:click={!visbeskjed && faaIs}/>
+	{#if visbeskjed}
+		<h1 transition:fly={{x:1000}}>{beskjed}</h1>
+	{:else}
+		<h1 transition:fly={{x:-1000}}>Kan jeg få is?</h1>
+	{/if}
+</main>
+
+<style>
+	:global(body){
+		margin:0;
+		padding:0
+	}
+	.set{
+		position:absolute;
+		width:100vw;
+		height:100vh;
+		display:grid;
+		place-items:center;
+		background:#4285F4;
+		z-index:1;
+	}
+	main {
+		display:grid;
+		place-items:center;
+		position:relative;
+		height:100vh;
+	}
+	img{
+		height:70%;
+	}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 3em;
+		font-weight: 100;
+		position:absolute;
+		bottom:2rem;
+	}
+</style>
---
+++ electron-svelte-template/src/App.svelte
@@ -0,0 +1,82 @@
+<script>
+	//context menu
+	const { remote } = require('electron')
+	const { Menu, MenuItem } = remote
+	let info = 'Nothing happening yet'
+
+		//notifications
+	const showNotification = () => {
+		let myNotification = new Notification('Hello', {
+		body: 'You are now officially part of the system OS'
+		})
+		myNotification.onclick = () => {
+			info = 'Notification clicked'
+		}
+		myNotification.show();
+	}
+
+	const menu = new Menu()
+	menu.append(new MenuItem({ 
+		label: 'meny 1', click() { 
+		info = 'item 1 klikket' } }))
+	menu.append(new MenuItem({ 
+		type: 'separator' }))
+	menu.append(new MenuItem({ 
+		label: 'meny 2', click() { 
+		info = 'item 2 klikket' } } ))
+
+
+	const context = e => {
+	e.preventDefault() 
+	menu.popup({ window: remote.getCurrentWindow() }) //referance: https://www.electronjs.org/docs/api/menu 
+		//for creating a menu dynamically in a web page 
+		//(render process) by using REMOTE module, and showing it 
+		//only when a user right clicks the page. 
+	}
+
+
+	const amIOnline = () => {
+		window.alert(navigator.onLine ? 'you\'re online sirs' : 'you\'re offline')
+		info = 'Alert accepted'
+	}
+</script>
+
+<main>
+	<h1>Svelte in Electron</h1>
+	<p>{info}</p>
+
+<button on:click={ () => showNotification() }>klikk meg</button>
+<button on:click={ () => amIOnline() }>Online</button>
+
+<div class="stuff" on:contextmenu={context}></div>
+
+
+	
+</main>
+
+<style>
+
+	.stuff {
+		width:100%;
+		height:200px;
+		background-color:slategray;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
